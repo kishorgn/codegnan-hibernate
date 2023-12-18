7ba346bceafa423418a6543499575daef0e9f1c9
kishor <kishor@kishor-ThinkPad-L14-Gen-2>
2023-12-18 17:24:07 +0530

    Commit - 2 : Cleaned pom.xml, refactored main method.

--- HibernateDemo_01/src/main/java/com/codegnan/App.java
+++ HibernateDemo_01/src/main/java/com/codegnan/App.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.SessionFactoryBuilder;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
@@ -18,10 +19,12 @@ public class App
 {
     public static void main( String[] args )
     {
-    	StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
-        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();  
-        SessionFactory sessionFactory = meta.getSessionFactoryBuilder()
-                                      .build();
+    	StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder();
+    	StandardServiceRegistry standardServiceRegistry = standardServiceRegistryBuilder.configure("hibernate.cfg.xml").build();
+    	MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
+        Metadata meta = metadataSources.getMetadataBuilder().build();
+        SessionFactoryBuilder sessionFactoryBuilder = meta.getSessionFactoryBuilder() ;
+        SessionFactory sessionFactory = sessionFactoryBuilder.build();
         
         Session session =  sessionFactory.openSession();
         session.beginTransaction();
