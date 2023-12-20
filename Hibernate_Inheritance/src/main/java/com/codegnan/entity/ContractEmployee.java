03837a0b04804ae97d3f90d75b20f347f4ec3788
kishor <kishor@kishor-ThinkPad-L14-Gen-2>
2023-12-20 15:29:49 +0530

    Commit - 8 : New project created for inheritance, and entities.

---
+++ Hibernate_Inheritance/src/main/java/com/codegnan/entity/ContractEmployee.java
@@ -0,0 +1,18 @@
+package com.codegnan.entity;
+
+public class ContractEmployee {
+	double payPerHr ;
+	String contractPeriod;
+	public double getPayPerHr() {
+		return payPerHr;
+	}
+	public void setPayPerHr(double payPerHr) {
+		this.payPerHr = payPerHr;
+	}
+	public String getContractPeriod() {
+		return contractPeriod;
+	}
+	public void setContractPeriod(String contractPeriod) {
+		this.contractPeriod = contractPeriod;
+	}
+}
---
+++ Hibernate_Inheritance/src/main/java/com/codegnan/entity/RegularEmployee.java
@@ -0,0 +1,18 @@
+package com.codegnan.entity;
+
+public class RegularEmployee {
+	double salary;
+	int bonus;
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public int getBonus() {
+		return bonus;
+	}
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+}
