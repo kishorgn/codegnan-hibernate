bffefd0e9156711858f347ba6e5cd1a77634177d
kishor <kishor@kishor-ThinkPad-L14-Gen-2>
2023-12-21 15:52:13 +0530

    Commit - 13 : Read operation done

---
+++ Hibernate_Collection/src/main/java/com/codegnan/App1.java
@@ -0,0 +1,58 @@
+package com.codegnan;
+
+import java.util.ArrayList;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.SessionFactoryBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.codegnan.entity.Question;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void main( String[] args )
+    {
+    	StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder();
+    	StandardServiceRegistry standardServiceRegistry = standardServiceRegistryBuilder.configure("hibernate.cfg.xml").build();
+    	MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
+        Metadata meta = metadataSources.getMetadataBuilder().build();
+        SessionFactoryBuilder sessionFactoryBuilder = meta.getSessionFactoryBuilder() ;
+        SessionFactory sessionFactory = sessionFactoryBuilder.build();
+        
+        Session session =  sessionFactory.openSession();
+        
+        session.beginTransaction();
+        
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.add("Java is a Platfor");
+        list1.add("Java is a programming language");
+        
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("Group of elements");
+        list2.add("One of the Java APIs");
+        
+        Question question1 = new Question();
+        question1.setQname("What is Java?");
+        question1.setAnswer(list1);
+        
+        Question question2 = new Question();
+        question2.setQname("What is Collection");
+        question2.setAnswer(list2);
+        
+        list1.add("Highly secured language");
+        
+        session.persist(question1);
+        session.persist(question2);
+        
+        session.getTransaction().commit();
+        session.close();
+    }
+}
--- Hibernate_Collection/src/main/java/com/codegnan/App2.java
+++ Hibernate_Collection/src/main/java/com/codegnan/App2.java
@@ -1,6 +1,10 @@
 package com.codegnan;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -16,7 +20,7 @@ import com.codegnan.entity.Question;
  * Hello world!
  *
  */
-public class App 
+public class App2
 {
     public static void main( String[] args )
     {
@@ -29,30 +33,16 @@ public class App
         
         Session session =  sessionFactory.openSession();
         
-        session.beginTransaction();
+        TypedQuery<Question> query = session.createQuery("from Question");
+        List<Question> questions = query.getResultList();
+        for(Question question : questions) {
+        	System.out.println("Question : "+question.getQname());
+        	List<String> answers = question.getAnswer();
+        	for(String answer : answers) {
+        		System.out.println(answer);
+        	}
+        }
         
-        ArrayList<String> list1 = new ArrayList<>();
-        list1.add("Java is a Platfor");
-        list1.add("Java is a programming language");
-        
-        ArrayList<String> list2 = new ArrayList<>();
-        list2.add("Group of elements");
-        list2.add("One of the Java APIs");
-        
-        Question question1 = new Question();
-        question1.setQname("What is Java?");
-        question1.setAnswer(list1);
-        
-        Question question2 = new Question();
-        question2.setQname("What is Collection");
-        question2.setAnswer(list2);
-        
-        list1.add("Highly secured language");
-        
-        session.persist(question1);
-        session.persist(question2);
-        
-        session.getTransaction().commit();
         session.close();
     }
 }
