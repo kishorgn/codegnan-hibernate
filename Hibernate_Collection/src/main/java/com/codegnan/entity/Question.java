8919866e9b94f786d29dec12ff3fce0d72d2d644
kishor <kishor@kishor-ThinkPad-L14-Gen-2>
2023-12-21 15:42:50 +0530

    Commit - 12 : Collection Mapping done with save.

---
+++ Hibernate_Collection/src/main/java/com/codegnan/entity/Question.java
@@ -0,0 +1,28 @@
+package com.codegnan.entity;
+
+import java.util.List;
+
+public class Question {
+	int id;
+	String qname;
+	List<String> answer;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getQname() {
+		return qname;
+	}
+	public void setQname(String qname) {
+		this.qname = qname;
+	}
+	public List<String> getAnswer() {
+		return answer;
+	}
+	public void setAnswer(List<String> answer) {
+		this.answer = answer;
+	}
+	
+}
