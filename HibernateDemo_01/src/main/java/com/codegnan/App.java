e591c373cda0a96581c62ab8e924f65bff923310
kishor <kishor@kishor-ThinkPad-L14-Gen-2>
2024-01-07 13:00:43 +0530

    Commit - 23 : After HQL Commands.

--- HibernateDemo_01/src/main/java/com/codegnan/App.java
+++ HibernateDemo_01/src/main/java/com/codegnan/App.java
@@ -2,8 +2,7 @@ package com.codegnan;
 
 import java.util.List;
 
-import javax.persistence.Query;
-
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
@@ -14,39 +13,31 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import com.codegnan.entity.Employee;
 
+public class App {
+	public static void main(String[] args) {
+		StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder();
+		StandardServiceRegistry standardServiceRegistry = standardServiceRegistryBuilder.configure("hibernate.cfg.xml")
+				.build();
+		MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
+		Metadata meta = metadataSources.getMetadataBuilder().build();
+		SessionFactoryBuilder sessionFactoryBuilder = meta.getSessionFactoryBuilder();
+		SessionFactory sessionFactory = sessionFactoryBuilder.build();
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder();
-    	StandardServiceRegistry standardServiceRegistry = standardServiceRegistryBuilder.configure("hibernate.cfg.xml").build();
-    	MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
-        Metadata meta = metadataSources.getMetadataBuilder().build();
-        SessionFactoryBuilder sessionFactoryBuilder = meta.getSessionFactoryBuilder() ;
-        SessionFactory sessionFactory = sessionFactoryBuilder.build();
-        
-        Session session =  sessionFactory.openSession();
-        
-        session.beginTransaction();
-        try {
-        	Employee employee = session.get(com.codegnan.entity.Employee.class, 101);
-        	
-        	session.delete(employee);
-            
-            session.getTransaction().commit();
-            System.out.println("Saved successfully");
-        	
-        	
-        }
-        catch(Exception e) {
-        	e.printStackTrace();
-        }
-        finally {
-        	session.close();
-            sessionFactory.close();
-            System.out.println("End of the program.");
-        }
-           
-    }
+		Session session = sessionFactory.openSession();
+		
+		session.getTransaction().begin();
+
+		Query query = session.createQuery("update Employee set firstName=:n where id=:id");
+		query.setParameter("n", "James");
+		query.setParameter("id", 103);
+		int noRec = query.executeUpdate();
+		System.out.println("No. records updated : "+noRec);
+		
+		session.getTransaction().commit();
+
+		session.close();
+		sessionFactory.close();
+		System.out.println("End of the program.");
+
+	}
 }
